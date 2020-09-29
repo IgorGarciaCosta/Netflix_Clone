a58f3a16ba58d206dbf36df30529c0c3283daffc
IgorGarciaCosta <igonildo7@gmail.com>
2020-09-29 19:18:15 -0300

    cover movie selector

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, {useEffect, useState} from 'react'
 import Tmdb from './Tmdb'
 import MovieRow from './components/MovieRow'
 import './App.css'
+import FeaturedMovie from './components/FeaturedMovie'
 
 export default()=>{
 
@@ -9,18 +10,31 @@ export default()=>{
   //movieList é como é count:0
   //setMovieList é como o setState no onClick de um button
   const [movieList, setMovieList] = useState([])
+  const [featuredData, setFeaturedData] = useState(null)
 
   useEffect(()=>{//quando a tela for carregada, roda essa função
     const loadAll = async()=>{
       //pega lista dos filmes
       let list = await Tmdb.getHomeList();
       setMovieList(list)
+
+      //pegar o filme em destaque (featured)
+      let originals = list.filter(i=>i.slug === 'originals')
+      //pegar um aleatorio dentro dos Originais
+      let randomChosen = Math.floor(Math.random()*(originals[0]).items.results.length-1)
+      let chosen = originals[0].items.results[randomChosen]
+      let chosenInfo = await Tmdb.getMovieInfo(chosen.id, 'tv')
+      setFeaturedData(chosenInfo)
+
     }
 
     loadAll()
   }, []);
   return(
     <div className = 'page'>
+
+      {featuredData && <FeaturedMovie item={featuredData}/>}
+
       <section className='lists'>
           {movieList.map((item, key)=>(
             <MovieRow key={key} title={item.title} items={item.items}/>
